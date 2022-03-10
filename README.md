ae3fd19fca4b9eec7a35b84c13fb27de32fcafb6
Lee Drake <b.lee.drake@gmail.com>
2022-03-10 12:01:49 -0700

    Vehicle Types
    Added statistics for vehicle types, both raw (e.g. Tanks, SAM, etc.) and synthetic (Armor, Air, Anti-air, etc.).

--- README.md
+++ README.md
@@ -4,16 +4,54 @@ Using data from [Oryx's site](https://www.oryxspioenkop.com/2022/02/attack-on-eu
 
 Data is drawn from [this public google sheet](https://docs.google.com/spreadsheets/d/1bngHbR0YPS7XH1oSA1VxoL4R34z60SJcR3NxguZM9GI/edit?usp=sharing)
 
-# Total Equipment Losses
-![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/current_total.jpg)
+## Total Equipment Losses
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_total.jpg)
 
-# Destroyed Equipment
-![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/current_destroyed.jpg)
+## Destroyed Equipment
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_destroyed.jpg)
 
-# Abandoned Equipment
-![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/current_abandoned.jpg)
+## Abandoned Equipment
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_abandoned.jpg)
 
-# Captured Equipment
-![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/current_captured.jpg)
+## Captured Equipment
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_captured.jpg)
 
+# Raw Equipment Losses
+"Raw" refers to a specific type of vehicle, such as a tank or armored personell carrier
+
+## Tanks
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_tanks.jpg)
+
+## Armored Fighting Vehicles (AFV)
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_afv.jpg)
+
+## Infantry Fighting Vehicles (AFV)
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_ifv.jpg)
+
+## Armored Personel Carriers (APC)
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_apc.jpg)
+
+## Infantry Mobility Vehicles (IMV)
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_imv.jpg)
+
+# Synthetic Equipment Losses
+"Synthetic" refers to a combination of vehicle types to form a theme - such as aircraft (fighters + helicopters + drones) or anti-aircraft (SAM + MANPADS), etc. 
+
+## Aircraft
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_aircraft.jpg)
+
+## Anti-air
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_antiair.jpg)
+
+## Armor
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_armor.jpg)
+
+## Infantry
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_infantry.jpg)
+
+## Vehicles (standard transportation)
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_vehicles.jpg)
+
+## Logistics Systems
+![alt text](https://raw.githubusercontent.com/leedrake5/Russia-Ukraine/master/Plots/current_logistics.jpg)
 
--- losses.r
+++ losses.r
@@ -25,7 +25,7 @@ scale_x_date(date_labels = "%m/%d") +
 scale_y_continuous("Total Equipment Losses") +
 ggtitle(paste0("Total equipment losses through ", Sys.Date())) +
 theme_light()
-ggsave("~/Github/Russia-Ukraine/current_total.jpg", current_total, device="jpg", width=6, height=5)
+ggsave("~/Github/Russia-Ukraine/Plots/current_total.jpg", current_total, device="jpg", width=6, height=5)
 
 ####Destroyed
 destroyed_melt <- melt(equipment_losses[,c("Date", "Russia_Destroyed", "Ukraine_Destroyed")], id.var="Date")
@@ -46,7 +46,7 @@ scale_x_date(date_labels = "%m/%d") +
 scale_y_continuous("Total Equipment Destroyed") +
 ggtitle(paste0("Total equipment destroyed through ", Sys.Date())) +
 theme_light()
-ggsave("~/Github/Russia-Ukraine/current_destroyed.jpg", current_destroyed, device="jpg", width=6, height=5)
+ggsave("~/Github/Russia-Ukraine/Plots/current_destroyed.jpg", current_destroyed, device="jpg", width=6, height=5)
 
 ####Abandoned
 abandoned_melt <- melt(equipment_losses[,c("Date", "Russia_Abandoned", "Ukraine_Abandoned")], id.var="Date")
@@ -66,7 +66,7 @@ scale_x_date(date_labels = "%m/%d") +
 scale_y_continuous("Total Equipment Abandoned") +
 ggtitle(paste0("Total equipment abandoned through ", Sys.Date())) +
 theme_light()
-ggsave("~/Github/Russia-Ukraine/current_abandoned.jpg", current_abandoned, device="jpg", width=6, height=5)
+ggsave("~/Github/Russia-Ukraine/Plots/current_abandoned.jpg", current_abandoned, device="jpg", width=6, height=5)
 
 ####Captured
 captured_melt <- melt(equipment_losses[,c("Date", "Russia_Captured", "Ukraine_Captured")], id.var="Date")
@@ -86,7 +86,7 @@ scale_x_date(date_labels = "%m/%d") +
 scale_y_continuous("Total Equipment Captured by Enemy") +
 ggtitle(paste0("Total equipment captured by enemy through ", Sys.Date())) +
 theme_light()
-ggsave("~/Github/Russia-Ukraine/current_captured.jpg", current_captured, device="jpg", width=6, height=5)
+ggsave("~/Github/Russia-Ukraine/Plots/current_captured.jpg", current_captured, device="jpg", width=6, height=5)
 
 
 ###All together now
@@ -111,4 +111,250 @@ ggtitle(paste0("Total equipment lost through ", Sys.Date())) +
 facet_grid(rows=vars(Type)) +
 theme_light() +
 theme(legend.position="bottom")
-ggsave("~/Github/Russia-Ukraine/current_grid.jpg", current_grid, device="jpg", width=6, height=10)
+ggsave("~/Github/Russia-Ukraine/Plots/current_grid.jpg", current_grid, device="jpg", width=6, height=10)
+
+
+####Raw Equipment Types
+####Tanks
+tanks_melt <- melt(equipment_losses[,c("Date", "Russia_Tanks", "Ukraine_Tanks")], id.var="Date")
+tanks_melt$Date <- as.Date(tanks_melt$Date, format="%m/%d/%Y")
+colnames(tanks_melt) <- c("Date", "Country", "Tanks")
+tanks_melt$Country <- gsub("_Tanks", "", tanks_melt$Country)
+tanks_melt <- tanks_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Tanks - lag(Tanks, default = first(Tanks)))
+
+current_tanks <- ggplot(tanks_melt, aes(Date, Tanks, colour=Country, shape=Country)) +
+geom_col(data=tanks_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Tanks Lost") +
+ggtitle(paste0("Tanks lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_tanks.jpg", current_tanks, device="jpg", width=6, height=5)
+
+####Armored Fighting Vehicles (AFVs)
+afv_melt <- melt(equipment_losses[,c("Date", "Russia_AFV", "Ukraine_AFV")], id.var="Date")
+afv_melt$Date <- as.Date(afv_melt$Date, format="%m/%d/%Y")
+colnames(afv_melt) <- c("Date", "Country", "AFV")
+afv_melt$Country <- gsub("_AFV", "", afv_melt$Country)
+afv_melt <- afv_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = AFV - lag(AFV, default = first(AFV)))
+
+current_afv <- ggplot(afv_melt, aes(Date, AFV, colour=Country, shape=Country)) +
+geom_col(data=afv_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Armored Fighting Vehicles Lost") +
+ggtitle(paste0("AFV lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_afv.jpg", current_afv, device="jpg", width=6, height=5)
+
+####Infantry Fighting Vehicles (AFVs)
+ifv_melt <- melt(equipment_losses[,c("Date", "Russia_IFV", "Ukraine_IFV")], id.var="Date")
+ifv_melt$Date <- as.Date(ifv_melt$Date, format="%m/%d/%Y")
+colnames(ifv_melt) <- c("Date", "Country", "IFV")
+ifv_melt$Country <- gsub("_IFV", "", ifv_melt$Country)
+ifv_melt <- ifv_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = IFV - lag(IFV, default = first(IFV)))
+
+current_ifv <- ggplot(ifv_melt, aes(Date, IFV, colour=Country, shape=Country)) +
+geom_col(data=ifv_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Infantry Fighting Vehicles Lost") +
+ggtitle(paste0("IFV lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_ifv.jpg", current_ifv, device="jpg", width=6, height=5)
+
+####Armored Personal Carriers
+apc_melt <- melt(equipment_losses[,c("Date", "Russia_APC", "Ukraine_APC")], id.var="Date")
+apc_melt$Date <- as.Date(apc_melt$Date, format="%m/%d/%Y")
+colnames(apc_melt) <- c("Date", "Country", "APC")
+apc_melt$Country <- gsub("_APC", "", apc_melt$Country)
+apc_melt <- apc_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = APC - lag(APC, default = first(APC)))
+
+current_apc <- ggplot(apc_melt, aes(Date, APC, colour=Country, shape=Country)) +
+geom_col(data=apc_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Armored Personal Carriers Lost") +
+ggtitle(paste0("APC lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_apc.jpg", current_apc, device="jpg", width=6, height=5)
+
+####Infantry Mobility Vehicles (IMVs)
+imv_melt <- melt(equipment_losses[,c("Date", "Russia_IMV", "Ukraine_IFV")], id.var="Date")
+imv_melt$Date <- as.Date(imv_melt$Date, format="%m/%d/%Y")
+colnames(imv_melt) <- c("Date", "Country", "IMV")
+imv_melt$Country <- gsub("_IMV", "", imv_melt$Country)
+imv_melt <- imv_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = IMV - lag(IMV, default = first(IMV)))
+
+current_imv <- ggplot(imv_melt, aes(Date, IMV, colour=Country, shape=Country)) +
+geom_col(data=imv_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Infantry Mobility Vehicles Lost") +
+ggtitle(paste0("IMV lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_imv.jpg", current_imv, device="jpg", width=6, height=5)
+
+####Engineering Vehicles (EVs)
+ev_melt <- melt(equipment_losses[,c("Date", "Russia_Engineering", "Ukraine_Engineering")], id.var="Date")
+ev_melt$Date <- as.Date(ev_melt$Date, format="%m/%d/%Y")
+colnames(ev_melt) <- c("Date", "Country", "EV")
+#ev_melt[ev_melt$EV==0] <- 0.1
+ev_melt$Country <- gsub("_Engineering", "", ev_melt$Country)
+ev_melt <- ev_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = EV - lag(EV, default = first(EV)))
+
+current_ev <- ggplot(ev_melt, aes(Date, EV, colour=Country, shape=Country)) +
+geom_col(data=ev_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Engineering Vehicles Lost") +
+ggtitle(paste0("EV lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_ev.jpg", current_ev, device="jpg", width=6, height=5)
+
+####Vehicles
+vehicles_melt <- melt(equipment_losses[,c("Date", "Russia_Vehicles", "Ukraine_Vehicles")], id.var="Date")
+vehicles_melt$Date <- as.Date(vehicles_melt$Date, format="%m/%d/%Y")
+colnames(vehicles_melt) <- c("Date", "Country", "Vehicles")
+vehicles_melt$Country <- gsub("_Vehicles", "", vehicles_melt$Country)
+vehicles_melt <- vehicles_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Vehicles - lag(Vehicles, default = first(Vehicles)))
+
+current_vehicles <- ggplot(vehicles_melt, aes(Date, Vehicles, colour=Country, shape=Country)) +
+geom_col(data=vehicles_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Vehicles Lost") +
+ggtitle(paste0("Vehicles lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_vehicles.jpg", current_vehicles, device="jpg", width=6, height=5)
+
+
+####Synthetic Units (combinations of equipment categories to highlight strategy)
+
+####Aircraft
+aircraft_melt <- melt(equipment_losses[,c("Date", "Russia_Aircraft", "Ukraine_Aircraft")], id.var="Date")
+aircraft_melt$Date <- as.Date(aircraft_melt$Date, format="%m/%d/%Y")
+colnames(aircraft_melt) <- c("Date", "Country", "Aircraft")
+aircraft_melt$Country <- gsub("_Aircraft", "", aircraft_melt$Country)
+aircraft_melt <- aircraft_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Aircraft - lag(Aircraft, default = first(Aircraft)))
+
+current_aircraft <- ggplot(aircraft_melt, aes(Date, Aircraft, colour=Country, shape=Country)) +
+geom_col(data=aircraft_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Aircraft Lost") +
+ggtitle(paste0("Aircraft lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_aircraft.jpg", current_aircraft, device="jpg", width=6, height=5)
+
+####Anti-Aircraft
+antiaircraft_melt <- melt(equipment_losses[,c("Date", "Russia_Antiair", "Ukraine_Antiair")], id.var="Date")
+antiaircraft_melt$Date <- as.Date(antiaircraft_melt$Date, format="%m/%d/%Y")
+colnames(antiaircraft_melt) <- c("Date", "Country", "Antiair")
+antiaircraft_melt$Country <- gsub("_Antiair", "", antiaircraft_melt$Country)
+antiaircraft_melt <- antiaircraft_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Antiair - lag(Antiair, default = first(Antiair)))
+
+current_antiair <- ggplot(antiaircraft_melt, aes(Date, Antiair, colour=Country, shape=Country)) +
+geom_col(data=antiaircraft_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Anti-Air Systems Lost") +
+ggtitle(paste0("Anti-air systems lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_antiair.jpg", current_antiair, device="jpg", width=6, height=5)
+
+####Infantry
+infantry_melt <- melt(equipment_losses[,c("Date", "Russia_Infantry", "Ukraine_Infantry")], id.var="Date")
+infantry_melt$Date <- as.Date(infantry_melt$Date, format="%m/%d/%Y")
+colnames(infantry_melt) <- c("Date", "Country", "Infantry")
+infantry_melt$Country <- gsub("_Infantry", "", infantry_melt$Country)
+infantry_melt <- infantry_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Infantry - lag(Infantry, default = first(Infantry)))
+
+current_infantry <- ggplot(infantry_melt, aes(Date, Infantry, colour=Country, shape=Country)) +
+geom_col(data=infantry_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Infantry Support Lost") +
+ggtitle(paste0("Infantry support lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_infantry.jpg", current_infantry, device="jpg", width=6, height=5)
+
+####Armor
+armor_melt <- melt(equipment_losses[,c("Date", "Russia_Armor", "Ukraine_Armor")], id.var="Date")
+armor_melt$Date <- as.Date(armor_melt$Date, format="%m/%d/%Y")
+colnames(armor_melt) <- c("Date", "Country", "Armor")
+armor_melt$Country <- gsub("_Armor", "", armor_melt$Country)
+armor_melt <- armor_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Armor - lag(Armor, default = first(Armor)))
+
+current_armor <- ggplot(armor_melt, aes(Date, Armor, colour=Country, shape=Country)) +
+geom_col(data=armor_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Armor Lost") +
+ggtitle(paste0("Armor support lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_armor.jpg", current_armor, device="jpg", width=6, height=5)
+
+####Logistics
+logistics_melt <- melt(equipment_losses[,c("Date", "Russia_Logistics", "Ukraine_Logistics")], id.var="Date")
+logistics_melt$Date <- as.Date(logistics_melt$Date, format="%m/%d/%Y")
+colnames(logistics_melt) <- c("Date", "Country", "Logistics")
+logistics_melt$Country <- gsub("_Logistics", "", logistics_melt$Country)
+logistics_melt <- logistics_melt %>%
+    group_by(Country) %>%
+    arrange(Date) %>%
+    mutate(Daily = Logistics - lag(Logistics, default = first(Logistics)))
+
+current_logistics <- ggplot(logistics_melt, aes(Date, Logistics, colour=Country, shape=Country)) +
+geom_col(data=logistics_melt, mapping=aes(Date, Daily, colour=Country,  fill=Country), alpha=0.8, position = position_dodge(0.7)) +
+geom_point() +
+stat_smooth() +
+scale_x_date(date_labels = "%m/%d") +
+scale_y_continuous("Logistics Systems Lost") +
+ggtitle(paste0("Logistics systems lost through ", Sys.Date())) +
+theme_light()
+ggsave("~/Github/Russia-Ukraine/Plots/current_logistics.jpg", current_logistics, device="jpg", width=6, height=5)
